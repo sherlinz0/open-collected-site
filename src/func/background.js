63e0bf9ddf0512186b3dad8735e69f6b8f850943
sherlinz0 <sherly_cs_sh@163.com>
2022-06-20 10:05:24 +0800

    Update: 修改语言首选项

--- src/func/background.js
+++ src/func/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.get(["collectedSites", "settings"], (res) => {
     const collectedSites = res.collectedSites || [];
     const settings = res.settings || {
-      language: 'zh'
+      language: 'en'
     };
 
     chrome.storage.sync.set({ collectedSites, settings });
